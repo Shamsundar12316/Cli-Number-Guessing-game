65559dcd9bdc47bee76343d520f6365a232375c5
Shamsundar12316 <168196650+Shamsundar12316@users.noreply.github.com>
2024-08-06 11:35:40 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,27 @@
+// GUESS GAME PROJECT
+
+
+import inquirer from  "inquirer";
+
+console.log("\n\tWellcome to Codewithsham - CLI Number Guessing Game\n");
+
+const randomNumber = Math.floor(Math.random() * 5
++ 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your guess number(Number Limit from 1 to 5):",
+    },
+
+
+]);
+
+if(answer.userGuessedNumber === randomNumber){
+    console.log("Congratulation ! You guess a correct number.");
+}
+else{
+    console.log("Sorry, you guess a wrong number");
+}
+
